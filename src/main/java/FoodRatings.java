6541ee047374e9ad6275360379aafdb2f31e1181
alek <wait82985@gmail.com>
2023-12-17 09:53:50 +0600

    initial commit

---
+++ src/main/java/FoodRatings.java
@@ -0,0 +1,14 @@
+public class FoodRatings {
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+
+    }
+
+    public void changeRating(String food, int newRating) {
+
+    }
+
+    public String highestRated(String cuisine) {
+
+    }
+}

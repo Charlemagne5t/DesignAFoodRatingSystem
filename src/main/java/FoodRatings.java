8550a9b451392dcc525a992cdf3c27fd15eb4b2b
alek <wait82985@gmail.com>
2023-12-17 15:34:23 +0600

    problem solved

--- src/main/java/FoodRatings.java
+++ src/main/java/FoodRatings.java
@@ -1,14 +1,57 @@
+import java.util.*;
+
 public class FoodRatings {
+    Map<String, PriorityQueue<String>> cuisinesPq = new HashMap<>();
+    Map<String, CuisineAndRating> foodsCuisine = new HashMap<>();
+
 
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
-
+        for (int i = 0; i < foods.length; i++) {
+            foodsCuisine.put(foods[i], new CuisineAndRating(cuisines[i], ratings[i]));
+            PriorityQueue<String> pq = cuisinesPq.getOrDefault(cuisines[i],
+                    new PriorityQueue<>(Comparator.comparingInt((String food) -> -foodsCuisine.get(food).rating).thenComparing(Comparator.naturalOrder())));
+            pq.offer(foods[i]);
+            cuisinesPq.put(cuisines[i], pq);
+        }
     }
 
     public void changeRating(String food, int newRating) {
-
+        CuisineAndRating cr = foodsCuisine.get(food);
+        foodsCuisine.remove(cr);
+        String cuisine = cr.cuisine;
+        foodsCuisine.put(food, new CuisineAndRating(cr.cuisine, newRating));
+        cuisinesPq.get(cuisine).remove(food);
+        cuisinesPq.get(cuisine).add(food);
     }
 
     public String highestRated(String cuisine) {
+        return cuisinesPq.get(cuisine).peek();
+    }
+}
+class CuisineAndRating{
+    String cuisine;
+    int rating;
 
+    public CuisineAndRating(String cuisine, int rating) {
+        this.cuisine = cuisine;
+        this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CuisineAndRating that = (CuisineAndRating) o;
+
+        if (rating != that.rating) return false;
+        return Objects.equals(cuisine, that.cuisine);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = cuisine != null ? cuisine.hashCode() : 0;
+        result = 31 * result + rating;
+        return result;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,2 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void test1(){
+        String[] foods = {"kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"};
+        String[] cuisines = {"korean", "japanese", "japanese", "greek", "japanese", "korean"};
+        int[] ratings = {9, 12, 8, 15, 14, 7};
+        FoodRatings foodRatings = new FoodRatings(foods, cuisines, ratings);
+        Assert.assertEquals("kimchi", foodRatings.highestRated("korean"));
+        Assert.assertEquals("ramen", foodRatings.highestRated("japanese"));
+        foodRatings.changeRating("sushi", 16);
+        Assert.assertEquals("sushi", foodRatings.highestRated("japanese"));
+        foodRatings.changeRating("ramen", 16);
+        Assert.assertEquals("ramen", foodRatings.highestRated("japanese"));
+
+    }
 }
